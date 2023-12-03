007ad0dfb7338c08e3ed135c8823541c836c626e
Benson Mwangi <kimaniben@gmail.com>
2023-12-03 08:23:24 -0500

    Completed assignenet

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,17 +1,33 @@
-import React from "react";
 
-function Filter({ onCategoryChange }) {
+import React, { useState, useEffect } from "react";
+
+function Filter({ onCategoryChange, onSearchChange, search }) {
+  const [searchText, setSearchText] = useState(search || ""); // Initialize with the value of the 'search' prop
+
+  useEffect(() => {
+    // Update the state when the 'search' prop changes
+    setSearchText(search || "");
+  }, [search]);
+
+
   return (
     <div className="Filter">
-      <input type="text" name="search" placeholder="Search..." />
+  
+      <input
+        type="text"
+        name="search"
+        placeholder="Search..."
+        value={searchText}
+        onChange={(e) => {
+          setSearchText(e.target.value);
+          onSearchChange(e.target.value);
+        }}
+      />
       <select name="filter" onChange={onCategoryChange}>
-        <option value="All">Filter by category</option>
-        <option value="Produce">Produce</option>
-        <option value="Dairy">Dairy</option>
-        <option value="Dessert">Dessert</option>
+      
+        {/* ... */}
       </select>
     </div>
   );
 }
-
 export default Filter;
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,24 +1,78 @@
+
 import React, { useState } from "react";
+import { v4 as uuid } from "uuid";
 
-function Item({ name, category }) {
-  const [isInCart, setIsInCart] = useState(false);
 
-  function handleAddToCartClick() {
-    setIsInCart((isInCart) => !isInCart);
-  }
+function ItemForm({ onItemFormSubmit }) {
+  // Initialize state for form inputs
+  const [itemName, setItemName] = useState("");
+  const [itemCategory, setItemCategory] = useState("Produce"); // Set initial category to "Produce"
+
+  // Handle input changes
+  const handleNameChange = (e) => {
+    setItemName(e.target.value);
+  };
+
+  const handleCategoryChange = (e) => {
+    setItemCategory(e.target.value);
+  };
+
+  // Handle form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Create a new item object
+    const newItem = {
+      id: uuid(),
+      name: itemName,
+      category: itemCategory,
+    };
+
+    // Pass the new item to the parent component via onItemFormSubmit prop
+    onItemFormSubmit(newItem);
+
+    // Reset form inputs using the callback form of state update functions
+    setItemName(() => "");
+    setItemCategory(() => "Produce");
+
+    // Continue with any other logic if needed
+  };
 
   return (
-    <li className={isInCart ? "in-cart" : ""}>
-      <span>{name}</span>
-      <span className="category">{category}</span>
-      <button
-        className={isInCart ? "remove" : "add"}
-        onClick={handleAddToCartClick}
-      >
-        {isInCart ? "Remove From" : "Add to"} Cart
+   
+    <form className="NewItem" onSubmit={handleSubmit}>
+      <label>
+        Name:
+     
+        <input
+          type="text"
+          name="name"
+          value={itemName}
+          onChange={handleNameChange}
+          data-testid="name-input"
+        />
+      </label>
+
+      <label>
+        Category:
+      
+        <select
+          name="category"
+          value={itemCategory}
+          onChange={handleCategoryChange}
+          data-testid="category-select"
+        >
+          <option value="Produce">Produce</option>
+          <option value="Dairy">Dairy</option>
+          <option value="Dessert">Dessert</option>
+        </select>
+      </label>
+
+
+      <button type="submit" data-testid="submit-button">
+        Add to List
       </button>
-    </li>
+    </form>
   );
 }
-
-export default Item;
+export default ItemForm;
